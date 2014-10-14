813957343788edf4059a9dd2e55a76c6a2fe4cec
Li Ge <lge@ucsd.edu>
2014-10-13 17:41:02 -0700

    initial commit

--- scenekit-test0/GameViewController.swift
+++ scenekit-test0/GameViewController.swift
@@ -9,14 +9,74 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import AVFoundation
 
 class GameViewController: UIViewController {
+    // interesting: tag = 0 won't work..
+    let SCNVIEW_TAG = 1000
+    let VIDEOVIEW_TAG = 2000
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initViews()
+        animateScene()
+        initControls()
+    }
+    
+    func initViews() {
+        let rootView = self.view
+        let scnView = createSceneView()
+        let scene = createScene()
+        scnView.scene = scene
+        let videoView = createVideoView()
+        rootView.addSubview(videoView)
+        rootView.addSubview(scnView)
+    }
+    
+    func createSceneView() -> SCNView {
+        let rootView = self.view
+        let scnView = SCNView()
+        scnView.frame = rootView.bounds
+        scnView.tag = SCNVIEW_TAG
+        scnView.backgroundColor = UIColor.clearColor()
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        return scnView
+    }
+    
+    func createVideoView() -> UIView {
+        let rootView = self.view
+        let videoView = UIView()
         
+        videoView.frame = rootView.bounds
+        videoView.tag = VIDEOVIEW_TAG
+        
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        let captureInput = AVCaptureDeviceInput.deviceInputWithDevice(device, error: nil) as AVCaptureDeviceInput
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSessionPresetMedium
+        
+        if captureSession.canAddInput(captureInput) {
+            captureSession.addInput(captureInput)
+        }
+        
+        let cameraVideoLayer = AVCaptureVideoPreviewLayer.layerWithSession(captureSession) as AVCaptureVideoPreviewLayer
+        captureSession.startRunning()
+        
+        cameraVideoLayer.frame = videoView.bounds
+        videoView.layer.addSublayer(cameraVideoLayer)
+        return videoView
+    }
+    
+    func createScene() -> SCNScene {
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.dae")
+        let scene = SCNScene()
+        let boxGeometry = SCNBox(width: 1.0, height: 2.0, length: 4.0, chamferRadius: 0.0)
+        let boxNode = SCNNode(geometry: boxGeometry)
+        boxNode.name = "box"
+        scene.rootNode.addChildNode(boxNode)
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
@@ -40,43 +100,50 @@ class GameViewController: UIViewController {
         ambientLightNode.light!.color = UIColor.darkGrayColor()
         scene.rootNode.addChildNode(ambientLightNode)
         
-        // retrieve the ship node
-        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.blackColor()
-        
+        return scene
+    }
+    
+    func getSceneView() -> SCNView {
+        return self.view.viewWithTag(SCNVIEW_TAG) as SCNView
+    }
+    
+    func getVideoView() -> UIView {
+        return self.view.viewWithTag(VIDEOVIEW_TAG)!
+    }
+    
+    func getScene() -> SCNScene {
+        return getSceneView().scene!
+    }
+    
+    func getRootNode() -> SCNNode {
+        return getScene().rootNode
+    }
+    
+    func getChildNodeByName(name: String) -> SCNNode? {
+        return getRootNode().childNodeWithName(name, recursively: true)
+    }
+    
+    func animateScene() {
+        let box = getChildNodeByName("box")
+        box!.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+    }
+    
+    func initControls() {
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
         let gestureRecognizers = NSMutableArray()
         gestureRecognizers.addObject(tapGesture)
-        if let existingGestureRecognizers = scnView.gestureRecognizers {
+        if let existingGestureRecognizers = self.view.gestureRecognizers {
             gestureRecognizers.addObjectsFromArray(existingGestureRecognizers)
         }
+        let scnView = getSceneView()
         scnView.gestureRecognizers = gestureRecognizers
     }
     
     func handleTap(gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as SCNView
-        
-        // check what nodes are tapped
+        let scnView = getSceneView()
         let p = gestureRecognize.locationInView(scnView)
+        
         if let hitResults = scnView.hitTest(p, options: nil) {
             // check that we clicked on at least one object
             if hitResults.count > 0 {
@@ -108,14 +175,14 @@ class GameViewController: UIViewController {
     }
     
     override func shouldAutorotate() -> Bool {
-        return true
+        return false
     }
     
     override func supportedInterfaceOrientations() -> Int {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
             return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
         } else {
-            return Int(UIInterfaceOrientationMask.All.toRaw())
+            return Int(UIInterfaceOrientationMask.Portrait.toRaw())
         }
     }
     
@@ -123,5 +190,5 @@ class GameViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
 }
